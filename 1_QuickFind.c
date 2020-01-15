d7fe70dea7199ac452f6722db4ee145c425a06ad
apoorvamalemath <35571354+apoorvamalemath@users.noreply.github.com>
2020-01-15 14:36:07 +0530

    Update 1_QuickFind.c

--- 1_QuickFind.c
+++ 1_QuickFind.c
@@ -1,6 +1,16 @@
 
 #include<stdio.h>
 
+/*
+	Union - To connect i.e. to merge components.
+	Find  - To check if connected or not.
+	
+	In every Union previously changed value must also be changed.
+
+	
+*/
+
+
 void Connected(int a[10],int p, int q)
 {
 	if(a[p]==a[q])	
@@ -25,6 +35,9 @@ void Union(int a[10], int p, int q)
 int main()
 {
 	int a[10];
+	inr i;
+	for(i=0;i<10;i++)
+		a[i]=i;
 	Union(a,1,2);
 	Union(a,3,5);
 	Union(a,1,5);
